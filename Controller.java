cf2836b051a569d79150b840d7fe3a774a4de78b
IbrahimSiddiqui007 <87603789+IbrahimSiddiqui007@users.noreply.github.com>
2021-12-15 12:47:34 +0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,4 +1,76 @@
-public class Controller
-{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable
+{
+    @FXML
+    private Button Clear;
+
+    @FXML
+    private Button Close_Bracket;
+
+    @FXML
+    private Button Cos;
+
+    @FXML
+    private Button Delete;
+
+    @FXML
+    private Button Log;
+
+    @FXML
+    private Button Open_Bracket;
+
+    @FXML
+    private Button Sin;
+
+    @FXML
+    private Button Tan;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Label resultDisplay;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    void btnActionHandler(ActionEvent event)
+    {
+        
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
