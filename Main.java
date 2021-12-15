ae03483a85041e228886a6e0da8b107d32026fd3
IbrahimSiddiqui007 <87603789+IbrahimSiddiqui007@users.noreply.github.com>
2021-12-15 12:47:59 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application
 {
@@ -20,7 +24,10 @@ public class Main extends Application
         {
             Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
             Scene scene = new Scene(root);
+            scene.setFill(Color.TRANSPARENT);
             stage.setScene(scene);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setResizable(false);
             stage.show();
         }
         catch (Exception e)
